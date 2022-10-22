603d68f4bcf95e1bb110bf7bc48d6ea2431ecfba
Andreas Moriah <amoriah@et-g5.kzn.21-school.ru>
2022-10-22 14:22:38 +0300

    first commit

---
+++ day01/datatypes.js
@@ -0,0 +1,5 @@
+console.log(str, "is", typeof str);
+console.log(tr, "is", typeof tr);
+console.log(fl, "is", typeof fl);
+console.log(undf, "is", typeof undf);
+console.log(nl, "is", typeof nl);
---
+++ day01/variable.js
@@ -0,0 +1,21 @@
+let str = "string",
+	tr = true,
+	fl = false,
+	undf = undefined,
+	nl = null;
+
+let var1;
+let var2;
+let var3;
+let var4;
+
+let val1 = 1;
+let val2 = 2;
+let val3 = 3;
+let val4 = 4;
+
+const first_name = 'Yana',
+		last_name = 'Onoyko';
+let fam_pos = 'single',
+ 	country = 'Russia',
+ 	age = 26;
