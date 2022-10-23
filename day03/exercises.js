a28080359e65e63f94c4a86c1a9a7ee08c683ac4
Andreas Moriah <amoriah@et-h5.kzn.21-school.ru>
2022-10-23 15:35:08 +0300

    add day3

---
+++ day03/exercises.js
@@ -0,0 +1,35 @@
+//-------------level_1-------------------------------------------------
+// let firstName, lastName, country, city, age, isMarried, year;
+
+// console.log(parseInt('9.8') == 10);
+
+// console.log(4 > 3)
+// console.log(4 >= 3)
+// console.log(4 < 3)
+// console.log(4 <= 3)
+// console.log(4 == 4)
+// console.log(4 === 4)
+// console.log(4 != 4)
+// console.log(4 !== 4)
+// console.log(4 != '4')
+// console.log(4 == '4')
+// console.log(4 === '4')
+// console.log('python'.length == 'jargon'.length);
+
+// console.log(4 > 3 && 10 < 12)
+// console.log(4 > 3 && 10 > 12)
+// console.log(!(4 > 3))
+// console.log(!(4 < 3))
+// console.log(!(4 === '4'))
+
+// const dt = new Date();
+// console.log(dt.getFullYear());
+// console.log(dt.getMonth());
+// console.log(dt.getDay());
+// console.log(dt.getDate());
+// console.log(dt.getMilliseconds());
+// console.log(dt.getMinutes());
+// console.log(dt.getHours());
+// console.log(dt.getTime());
+
+//-------------level_2-------------------------------------------------
---
+++ day03/script.js
@@ -0,0 +1,29 @@
+//----------work with Date-------------------
+
+// const now = new Date();
+// console.log(now); // 
+
+// const now = new Date();
+// console.log(now.getFullYear()); // 2020
+
+// const now = new Date();
+// console.log(now.getMonth());
+
+// const now = new Date();
+// console.log(now.getDate()); 
+
+// const now = new Date();
+// console.log(now.getDay()); // 6, потому, что день Суббота, который является 5-м днем,
+// // Получение дня недели в виде числа (0-6)
+
+// const now = new Date();
+// console.log(now.getHours());
+
+// const now = new Date();
+// console.log(now.getMinutes()); 
+
+// const now = new Date();
+// console.log(now.getSeconds())
+
+const now = new Date(); //
+console.log(now.getTime());
