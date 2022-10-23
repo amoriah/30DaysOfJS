2facd119538e9e08442ca36782a7e9ce8cd8ca44
Andreas Moriah <amoriah@et-h5.kzn.21-school.ru>
2022-10-23 16:51:54 +0300

    finished day3

--- day03/exercises.js
+++ day03/exercises.js
@@ -33,3 +33,35 @@
 // console.log(dt.getTime());
 
 //-------------level_2-------------------------------------------------
+// area of a triangle
+// let base = prompt("Enter base", 0);
+// let height = prompt("Enter height", 0);
+// alert(`The area of the triangle is ${0.5 * base * height}`);
+
+// perimeter of triangle 
+// let a = Number(prompt("Enter side a", 0));
+// let b = Number(prompt("Enter side b", 0));
+// let c = Number(prompt("Enter side c", 0));
+// alert(`The perimeter of the triangle is ${a + b + c}`);
+
+// area of rectangle
+// let a = Number(prompt("Enter side a", 0));
+// let b = Number(prompt("Enter side b", 0));
+// alert(`The  area of rectangle is ${2 * (a + b)}`);
+
+// Calculate pay of the person
+// let h = Number(prompt("Enter hours:", 0));
+// let r = Number(prompt("Enter rate per hour:", 0));
+// alert(`Your weekly earning is ${h * r}`);
+
+//Compare name length
+// let firstName = prompt("firstName:", 0);
+// let lastName = prompt("lastName:", 0);
+// alert(`Your first_name, ${firstName} is ${firstName.length > lastName.length? 'longer' : 'shorter'} than your famil_name, ${lastName}`)
+
+// time format
+// const tm = new Date();
+// console.log(`${tm.getFullYear()}-${tm.getMonth()}-${tm.getDate()} ${tm.getHours()}:${tm.getSeconds()}`);
+
+
+
