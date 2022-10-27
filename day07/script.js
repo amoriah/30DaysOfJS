f091ba4b242eb7417fe7b1e5f1974a35772f14e5
Andreas Moriah <amoriah@va-g5.kzn.21-school.ru>
2022-10-27 19:37:41 +0300

    work with shuffle algm

--- day07/script.js
+++ day07/script.js
@@ -96,7 +96,7 @@ function sumOfOdds(numbers){
 	let sum = 0;
 	numbers.forEach((num) => { 
 		if (num % 2 != 0)
-		sum += num;
+			sum += num;
 	})
 	return sum;
 }
@@ -113,8 +113,7 @@ function sumOfEven(numbers){
 // console.log(sumOfEven([1,2,3,4,5,6]));
 //------------------------------------------------------------------------------
 function evensAndOdds(number){
-	let odds = 0, evens = 0;
-	let i = -1;
+	let odds = 0, evens = 0, i = -1;
 	while (++i <= number){
 		if (i % 2 == 0)
 			evens++;
@@ -149,9 +148,9 @@ function randomMacAddress(){
 	let adress = '';
 	for (let j = 0; j < 6; j++){
 		for (let i = 0; i < 2; i++)
-		adress += hex[Math.floor(Math.random() * hex.length)];
+			adress += hex[Math.floor(Math.random() * hex.length)];
 		if (j == 5)
-		break;
+			break;
 		adress += '-';
 	}
 	return adress;	
@@ -163,7 +162,7 @@ function randomHexaNumberGenerator(){
 	const hex = '0123456789ABCDEF';
 	let num = '';
 	for(let i = 0; i < 6; i++)
-	num += hex[Math.floor(Math.random() * hex.length)];
+		num += hex[Math.floor(Math.random() * hex.length)];
 	return '#' + num;
 }
 
@@ -179,7 +178,87 @@ function userIdGeneratedByUser(){
 	let countChar = prompt('enter count of character:');
 	let countId = prompt('enter count of id:');
 	for (let i = 0; i < countId; i++)
-		console.log(`${(~~(Math.random() *1e8)).toString(16)}`);
+		console.log(`${(~~(Math.random() * (1e9 * 1e9))).toString(16).toUpperCase().substring(0, countChar)}`);
 }
 
-userIdGeneratedByUser();
+// userIdGeneratedByUser();
+//-------------------------------------------------------------------------------
+function rgbColorGenerator(){
+	return `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
+}
+// console.log(rgbColorGenerator())
+//rgb(125,244,255)
+//------------------------------------------------------------------------------
+function arrayOfHexaColors(N){
+	let colors = [];
+	for (let i = 0; i < N; i++)
+		colors.push('#' + (Math.floor(Math.random() * 1e9).toString(16).substring(0,6)));
+	return colors;
+}
+
+// console.log(arrayOfHexaColors(3))
+//----------------------------------------------------------------------------------
+function arrayOfRgbColors(N){
+	let colors = [];
+	for (let i = 0; i < N; i++)
+		colors.push(`rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`);
+	return colors;
+}
+
+// console.log(arrayOfRgbColors(10))
+//--------------------------------------------------------------------------------
+function convertHexaToRgb(hex){
+	let r, g, b;
+	r = parseInt(hex.substring(1,3), 16);
+	g = parseInt(hex.substring(3,5), 16)
+	b = parseInt(hex.substring(5,7), 16)
+	return `rgb(${r},${g},${b})`;
+}
+
+// console.log(convertHexaToRgb('#a7bf40'));
+//------------------------------------------------------------------------------
+//нужно добавить ноль к числам < 16
+function convertRgbToHexa(rgb){
+	let tmp = rgb.split(',');
+	let rgb_n = [];
+	for (let el in tmp)
+		rgb_n.push(tmp[el].match(/\d+/gi));
+	let r = Number(rgb_n[0]).toString(16);
+	let g = Number(rgb_n[1]).toString(16);
+	let b = Number(rgb_n[2]).toString(16);
+	return `#${r}${g}${b}`;
+}
+// console.log(convertRgbToHexa('rgb(255,200,130)'));
+//---------------------------------------------------------------------------
+function shuffleArray(arr){
+	let len = arr.length;
+	let shuffle = [];
+	for (let i = 0; i < len; i++)
+		shuffle.push(arr[~~(Math.random() * len)])
+	return shuffle;
+}
+
+// console.log(shuffleArray(['a','b','c','d','e','f','g']))
+
+function shuffle(array) {
+	let currentIndex = array.length,  randomIndex;
+  
+	// While there remain elements to shuffle.
+	while (currentIndex != 0) {
+  
+	  // Pick a remaining element.
+	  randomIndex = Math.floor(Math.random() * currentIndex);
+	  currentIndex--;
+  
+	  // And swap it with the current element.
+	  [array[currentIndex], array[randomIndex]] = [
+		array[randomIndex], array[currentIndex]];
+	}
+  
+	return array;
+  }
+  
+  // Used like so
+  var arr_m = [2, 11, 37, 42];
+  shuffle(arr_m);
+  console.log(arr_m);
